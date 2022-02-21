7e9f56ade6108555aa5cb0446aa33bdbc0fac7fb
John Liu <john831118@gmail.com>
2022-02-21 11:42:16 +0800

    step 2: add `ProductsView`

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <template v-for="product in products" :key="product">
+            {{product.name}} <br>
+            {{product.category}} <br>
+        </template>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return{
+            products:[]
+        }
+    },
+    mounted(){
+        axios.get(`http://35.234.34.149/products?user_id=1`)
+            .then(response => this.products = response.data)
+    }
+}
+</script>
+<style scoped>
+
+</style>
