22b4cca80b590f19e32c444c626eed87b17a5888
John Liu <john831118@gmail.com>
2022-02-21 15:58:07 +0800

    step 7: add `vue-persistedstate` and currentQuantity

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 export default createStore({
   state: {
@@ -6,31 +7,36 @@ export default createStore({
     cart: []
   },
   getters: {
+    currentQuantity(state){
+      let total = 0;
+      for(var i = 0; i < state.cart.length; i++){
+        total += state.cart[i].number
+      }
+      return total
+    }
   },
   mutations: {
     addCart(state, data){
       let isNewProduct = true
-      console.log(data)
       state.cart.map(function(product){
-        console.log(product)
         if(product.id === data.product.id){
           product.number += data.number
           isNewProduct = false
         }
         return product
       })
-      console.log(isNewProduct)
       if(isNewProduct){
         let newProduct = data.product
         newProduct.number = data.number
         state.cart.push(newProduct)
       }
-
-      console.log(state.cart)
     }
   },
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [
+    createPersistedState()
+  ]
 })
