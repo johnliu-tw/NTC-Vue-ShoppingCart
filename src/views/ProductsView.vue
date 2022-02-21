a635bedfe3e878bcb7e97ad2e5f680fcbd069fa4
John Liu <john831118@gmail.com>
2022-02-21 12:16:00 +0800

    step 3: `ProductsView` styling

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,8 +1,14 @@
 <template>
-    <div>
+    <div class="row">
         <template v-for="product in products" :key="product">
-            {{product.name}} <br>
-            {{product.category}} <br>
+            <div class="product-box">
+                <div class="image-box">
+                    <img :src="parseImgPath(product.img_path)">
+                </div>
+                <h4>{{product.brand}} <small>{{product.category}}</small></h4>
+                <h3>{{product.name}}</h3>
+                <p>售價: {{ product.price }}</p>
+            </div>
         </template>
     </div>
 </template>
@@ -17,9 +23,40 @@ export default {
     mounted(){
         axios.get(`http://35.234.34.149/products?user_id=1`)
             .then(response => this.products = response.data)
+    },
+    methods:{
+        parseImgPath: function(path){
+            return `http://35.234.34.149${path}`;
+        }
     }
+
 }
 </script>
 <style scoped>
-
+    div.row{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        width: 80%;
+        margin: auto;
+    }
+    div.product-box{
+        border: 1px solid #888888;
+        min-width: 350px;
+        width: 24%;
+        text-align: center;
+        margin: 1px;
+        cursor: pointer;
+        display: block;
+    }
+    div.image-box{
+        width: 100%;
+        height: 250px;
+        display: flex;
+    }
+    img{
+        display: block;
+        width: 50%;
+        margin: auto;
+    }
 </style>
